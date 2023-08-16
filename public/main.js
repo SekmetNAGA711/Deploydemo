c449634783a9a555af9202020d1344a562eea261
Abby Acheampong <abenaacheampong@abenas-mbp.myfiosgateway.com>
2023-08-16 13:34:45 -0400

    revision to main.js and server.js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,7 @@
 const btn = document.querySelector('button')
 
 const clickHandler = () => {
-    axios.get('/api/cat')
-    .then((res)=>alert(res.data))
-    .catch((thesePaws) => alert("there was turbulance"))
+  axios.get('/api/cat').then((res) => alert(res.data)).catch((err)=> alert("There was turbulance"))
 }
 
 btn.addEventListener('click', clickHandler)
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
-app.get('api/cat', (req,res) => {
-    res.status(200).send('Best regards from Flavio')
+app.get('/api/cat', (req,res) => {
+ res.status(200).send('Best regards flavio')
 })
+
 app.listen(4000, () => console.log("server running on 4000"))
